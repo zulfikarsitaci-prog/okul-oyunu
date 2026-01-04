7c19e5db18446c421514b0406b484254a1749ff2
zulfikarsitaci-prog <zulfikar.sitaci@gmail.com>
2026-01-05 02:08:10 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 import hashlib
 
 def connect():
-    return sqlite3.connect('education_platform.db')
+    return sqlite3.connect('education_platform.db', check_same_thread=False)
 
 def create_database():
     conn = connect()
@@ -18,6 +18,10 @@ def add_user(username, password, role):
     conn = connect()
     cursor = conn.cursor()
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", (username, hashed_password, role))
-    conn.commit()
-    conn.close()
+    try:
+        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", (username, hashed_password, role))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Hata: {e}")
+    finally:
+        conn.close()
