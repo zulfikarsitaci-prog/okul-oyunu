44eed59b9b4761d28fd996a2bff44cf98aa96885
zulfikarsitaci-prog <zulfikar.sitaci@gmail.com>
2026-01-05 01:40:29 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 def create_database():
     conn = sqlite3.connect('education_platform.db')
@@ -13,3 +14,14 @@ def create_database():
     ''')
     conn.commit()
     conn.close()
+
+def add_user(username, password, role):
+    conn = sqlite3.connect('education_platform.db')
+    cursor = conn.cursor()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", (username, hashed_password, role))
+    conn.commit()
+    conn.close()
+
+def connect():
+    return sqlite3.connect('education_platform.db')
